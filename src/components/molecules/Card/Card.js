5c593ce68cbcc16db47141c0147f17d9b72e255a
Ekaterina <ekaterinatihonenko8@gmail.com>
2023-03-08 19:56:10 +0300

    main

--- src/components/molecules/Card/Card.js
+++ src/components/molecules/Card/Card.js
@@ -11,10 +11,14 @@ class Card extends Component {
             <div class="card">
                <img class="image-fit" src="${image}" alt="image">
                <div class="card-body">
-                  <h5 class="card-title">${title}</h5>
-                  <p class="card-text">${description}</p>
-                  <small class="card-title pricing-card-title">${price}$</small>
-                  <a href="#" class="btn btn-primary">Go somewhere</a>
+                  <div class="container">
+                     <h5 class="card-title">${title}</h5>
+                     <p class="card-text">${description}</p>
+                  </div>
+                  <div class="container d-flex align-items-center">
+                     <small class="card-title pricing-card-title m-0">${price}$</small>
+                     <a href="#" class="btn btn-primary m-2 p-1">Go somewhere</a>
+                  </div>
                </div>
             </div>
       `;
--- src/components/pages/index.js
+++
@@ -1,7 +0,0 @@
-/*export * from './CatalogPage';
-export * from './ShoppingCartPage';
-export * from './ErrorPage';
-export * from './BlogPage';
-export * from './ContactsPage';
-export * from './ProductPage';
-*/
